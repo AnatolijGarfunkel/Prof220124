c2d9027bbe3bc1dfb8537ca90a26212b8ef32f33
tolya <tolya@gmx.de>
2025-10-15 16:18:17 +0200

    Queue example

--- src/main/java/lesson_09/QueueTester.java
+++ src/main/java/lesson_09/QueueTester.java
@@ -1,4 +1,18 @@
 package lesson_09;
 
-public class QueueTester {
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class QueueTester  {
+
+    public static void main(String[] args) {
+
+        List<String> list = List.of("eins", "zwei", "drei", "vier");
+
+        Queue<String> bankQueue = new LinkedList<>(list);
+
+        while (!bankQueue.isEmpty())
+            System.out.printf(bankQueue.remove() + " ");
+    }
 }
