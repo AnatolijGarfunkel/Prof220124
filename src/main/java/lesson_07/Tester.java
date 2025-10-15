960a1a380141e7da7f7a7f1d46f772c72d812929
tolya <tolya@gmx.de>
2025-10-15 15:23:33 +0200

    lesson_07 Tester

--- src/main/java/lesson_07/Tester.java
+++ src/main/java/lesson_07/Tester.java
@@ -1,12 +1,25 @@
 package lesson_07;
 
+import java.util.Iterator;
+
 public class Tester {
 
     public static void main(String[] args) {
 
         MyLinkedList myLinkedList = new MyLinkedListImpl();
 
-        System.out.println("myList");
+        myLinkedList.add(0);
+        myLinkedList.add(1);
+        myLinkedList.add(2);
+
+        System.out.println("Size: " + myLinkedList.size());
         System.out.println(myLinkedList);
+
+        myLinkedList.addFirst(9);
+
+
+        System.out.println("Size: " + myLinkedList.size());
+        System.out.println(myLinkedList);
+
     }
 }
