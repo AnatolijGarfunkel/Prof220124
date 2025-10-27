48a0a5bb1bfad5baf2007a82ac6a305250f4fa56
tolya <tolya@gmx.de>
2025-10-27 15:55:55 +0100

    equals and hashCode

---
+++ src/main/java/lesson_10/Cat.java
@@ -0,0 +1,59 @@
+package lesson_10;
+
+import java.util.Objects;
+
+public class Cat {
+
+    private String name;
+
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean myEquals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null)
+            return false;
+
+        if (!(obj instanceof Cat cat))
+            return false;
+
+        if (this.hashCode() != obj.hashCode())
+            return false;
+
+        return this.name.equals(cat.name) && this.age == cat.age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return age == cat.age && Objects.equals(name, cat.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
