3e48a29bb353d391552edc392f9c3be1c1e0d444
tolya <tolya@gmx.de>
2025-10-27 14:53:13 +0100

    vorhanden(String string

--- src/main/java/lesson_09/HomeWork.java
+++ src/main/java/lesson_09/HomeWork.java
@@ -6,11 +6,10 @@ public class HomeWork {
 
     public static void main(String[] args) {
 
-        Set<Integer> set = new HashSet<>(Set.of(-1, 12, 3, 4, -8, -2));
+        String string = "один раз это один раз но только раз";
 
-        removeNegative(set);
-
-        System.out.println("result " + set);
+        Map<String, List<Integer>> map = vorhanden(string);
+        System.out.println(map);
     }
 
     public static Set<Character> unique(String word) {
@@ -25,5 +24,21 @@ public class HomeWork {
     public static void removeNegative(Set<Integer> set) {
 
         set.removeIf(integer -> integer < 0);
+    }
+
+    public static Map<String, List<Integer>> vorhanden(String string) {
+        Map<String, List<Integer>> map = new LinkedHashMap<>();
+        List<String> list = List.of(string.split(" "));
+        Set<String> set = new LinkedHashSet<>(list);
+
+        for (String data: set) {
+            List<Integer> count = new LinkedList<>();
+            for (int i = 0; i < list.size(); i ++) {
+                if (data.equals(list.get(i)))
+                    count.add(i);
+            }
+            map.put(data, count);
+        }
+        return map;
     }
 }
