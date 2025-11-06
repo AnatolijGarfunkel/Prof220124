411cb313b54d76a09e70c2805af239d39b43e327
tolya <tolya@gmx.de>
2025-11-06 12:23:52 +0100

    capitals

--- src/main/java/lesson_11/MapTester.java
+++ src/main/java/lesson_11/MapTester.java
@@ -1,8 +1,63 @@
 package lesson_11;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapTester {
 
     public static void main(String[] args) {
+        Map<String, String> capitals = new HashMap<>();
+        capitals.put("Germany", "Berlin");
+        capitals.put("France", "Paris");
+        System.out.println(capitals);
+
         
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
