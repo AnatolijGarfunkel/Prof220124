9780d427454e4a15ea89d82254e1a16067a0d6c2
tolya <tolya@gmx.de>
2025-11-06 11:09:31 +0100

    sort

--- src/main/java/lesson_10/ComparatorTester.java
+++ src/main/java/lesson_10/ComparatorTester.java
@@ -1,4 +1,19 @@
 package lesson_10;
 
+import java.util.*;
+
 public class ComparatorTester {
+
+    public static void main(String[] args) {
+
+        List<String> groups = new ArrayList<>(
+                Arrays.asList(
+                        "Ramones", "Aerosmith", "Rolling Stones", "Beatles", "Roxette", "INXS"
+                )
+        );
+
+        System.out.println(groups);
+        groups.sort((o1, o2) -> o1.length() - o2.length());
+        System.out.println(groups);
+    }
 }
