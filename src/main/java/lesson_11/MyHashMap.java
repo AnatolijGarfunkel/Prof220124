49241935bf35da7decd26c6059208e6566fd8e60
tolya <tolya@gmx.de>
2025-11-09 16:56:42 +0100

    MyHashMap

---
+++ src/main/java/lesson_11/MyHashMap.java
@@ -0,0 +1,32 @@
+package lesson_11;
+
+public class MyHashMap implements MyMap{
+
+
+
+
+    @Override
+    public void put(String k, String v) {
+
+    }
+
+    @Override
+    public String get(String k) {
+        return "";
+    }
+
+    @Override
+    public String remove(String k) {
+        return "";
+    }
+
+    @Override
+    public boolean contains(String k) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
