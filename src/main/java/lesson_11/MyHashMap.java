be998335b7c8100e805329fddc817097ebafc43e
tolya <tolya@gmx.de>
2025-11-10 18:33:38 +0100

    resize()

--- src/main/java/lesson_11/MyHashMap.java
+++ src/main/java/lesson_11/MyHashMap.java
@@ -18,6 +18,13 @@ public class MyHashMap implements MyMap {
     @Override
     public void put(String k, String v) {
         Pair pair = findPair(k);
+        if (pair != null) {
+            pair.setValue(v);
+            return;
+        }
+        if (size() > LOAD_FACTOR * capacity()) {
+            resize(k, v);
+        }
         if (pair == null) {
             int bucket = findBucket(k);
             if (source[bucket] == null) {
@@ -30,7 +37,55 @@ public class MyHashMap implements MyMap {
                     pair = pair.getNext();
                 pair.setNext(new Pair(k, v, null));
             }
+            size ++;
         }
+    }
+
+    private void resize(String k, String v) {
+        Pair[] newSource = new Pair[capacity() * 2];
+        size = 0;
+
+        for (Pair data : source) {
+            if (data != null) {
+                while (data != null) {
+                    rePut(newSource, data.getKey(), data.getValue());
+                    data = data.getNext();
+                }
+            }
+        }
+        source = newSource;
+    }
+
+    private void rePut(Pair[] newSource, String key, String value) {
+        Pair pair = reFindPair(newSource, key);
+        if (pair != null) {
+            pair.setValue(value);
+            return;
+        }
+        if (pair == null) {
+            int bucket = Math.abs(key.hashCode() % (capacity() * 2));
+            if (newSource[bucket] == null) {
+                pair = new Pair(key, value, null);
+                newSource[bucket] = pair;
+            } else {
+                pair = newSource[bucket];
+                while (pair.getNext() != null)
+                    pair = pair.getNext();
+                pair.setNext(new Pair(key, value, null));
+            }
+            size++;
+        }
+    }
+
+    private Pair reFindPair(Pair[] newSource, String key) {
+        int bucket = Math.abs(key.hashCode() % (capacity() * 2));
+        Pair pair = newSource[bucket];
+        while (pair != null) {
+            if (pair.getKey().equals(key))
+                return pair;
+            pair = pair.getNext();
+        }
+        return null;
     }
 
     @Override
@@ -73,15 +128,13 @@ public class MyHashMap implements MyMap {
 
     @Override
     public String toString() {
-        String result = "{\n\t";
+        String result = "\n";
         for (int i = 0; i < source.length; i ++) {
             if (source[i] != null) {
                 Pair pair = source[i];
                 while (pair != null) {
                     result += "[" + i + "]";
-                    if (pair.getNext() == null && i < source.length - 1)
-                        result += "[" + pair.getKey() + " : " + pair.getValue() + "]\n\t";
-                    else if (pair.getNext() == null && i == source.length - 1)
+                    if (pair.getNext() == null)
                         result += "[" + pair.getKey() + " : " + pair.getValue() + "]\n";
                     else
                         result += "[" + pair.getKey() + " : " + pair.getValue() + "], ";
@@ -89,7 +142,6 @@ public class MyHashMap implements MyMap {
                 }
             }
         }
-        result += "}";
         return result;
     }
 }
