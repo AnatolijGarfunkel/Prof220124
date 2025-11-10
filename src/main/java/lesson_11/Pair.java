de676a2bb27bc59e0e4c54e42c3d4129f3a4ec32
tolya <tolya@gmx.de>
2025-11-10 12:02:03 +0100

    Pair - vars, konst, toString

--- src/main/java/lesson_11/Pair.java
+++ src/main/java/lesson_11/Pair.java
@@ -5,4 +5,17 @@ public class Pair {
     private String key;
 
     private String value;
+
+    private Pair next;
+
+    public Pair(String key, String value, Pair next) {
+        this.key = key;
+        this.value = value;
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return key + " : " + value;
+    }
 }
