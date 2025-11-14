685df4c4262c6133e71b5b8f65740d555cf67898
tolya <tolya@gmx.de>
2025-11-14 16:56:53 +0100

    StringValidator

---
+++ src/main/java/lesson_12_functional_interfaces/string_validator/StringValidator.java
@@ -0,0 +1,19 @@
+package lesson_12_functional_interfaces.string_validator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringValidator {
+
+    private List rules = new ArrayList();
+
+    public void addRule(StringRule rule) {
+
+
+    }
+
+    public List validates(String value) {
+        
+        return null;
+    }
+}
