9bc87dab12a270f855f1a0f99d5f1d1e003ce147
tolya <tolya@gmx.de>
2025-11-14 08:24:08 +0100

    ValidationStatus

---
+++ src/main/java/lesson_12_functional_interfaces/ValidationStatus.java
@@ -0,0 +1,12 @@
+package lesson_12_functional_interfaces;
+
+public enum ValidationStatus {
+
+    OK,
+    FIELD_NULL,
+    VALIDATORS_NULL,
+    VALIDATORS_EMPTY,
+    VALIDATOR_NULL,
+    RULE_VIOLATION
+    
+}
