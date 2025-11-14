23f000fd707292c1ff0d1c84b9771dc9aa572579
tolya <tolya@gmx.de>
2025-11-14 17:17:54 +0100

    addRule(), validate()

--- src/main/java/lesson_12_functional_interfaces/string_validator/StringValidator.java
+++ src/main/java/lesson_12_functional_interfaces/string_validator/StringValidator.java
@@ -5,15 +5,19 @@ import java.util.List;
 
 public class StringValidator {
 
-    private List rules = new ArrayList();
+    private List<StringRule> rules = new ArrayList();
 
     public void addRule(StringRule rule) {
-
-
+        rules.add(rule);
     }
 
-    public List validates(String value) {
-
-        return null;
+    public List<String> validates(String value) {
+        List<String> result = new ArrayList<>();
+        for (StringRule rule : rules) {
+            String validated = rule.validate(value);
+            if (!validated.equals("null"))
+                result.add(validated);
+        }
+        return result;
     }
 }
