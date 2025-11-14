89a322e8c1b8c27d7e1101c4ed2fa33a073d8ce6
tolya <tolya@gmx.de>
2025-11-14 17:25:05 +0100

    notNull(), notEmpty(), minLenth()

--- src/main/java/lesson_12_functional_interfaces/string_validator/StringRules.java
+++ src/main/java/lesson_12_functional_interfaces/string_validator/StringRules.java
@@ -3,17 +3,26 @@ package lesson_12_functional_interfaces.string_validator;
 public class StringRules {
 
     public static StringRule notNull() {
-
-        return null;
+        return value -> {
+            if (value == null)
+                return "Wert darf nicht null sein";
+            return null;
+        };
     }
 
     public static StringRule notEmpty() {
-
-        return null;
+        return value -> {
+            if (value != null && value.isEmpty())
+                return "Wert darf nicht leer sein";
+            return null;
+        };
     }
 
     public static StringRule minLength(int length) {
-
-        return null;
+        return value -> {
+            if (value != null && value.length() < length)
+                return "Wert muss mindestens " + length + " Zeichen lang sein";
+            return null;
+        };
     }
 }
--- src/main/java/lesson_12_functional_interfaces/string_validator/StringValidator.java
+++ src/main/java/lesson_12_functional_interfaces/string_validator/StringValidator.java
@@ -5,17 +5,17 @@ import java.util.List;
 
 public class StringValidator {
 
-    private List<StringRule> rules = new ArrayList();
+    private final List<StringRule> rules = new ArrayList<>();
 
     public void addRule(StringRule rule) {
         rules.add(rule);
     }
 
-    public List<String> validates(String value) {
+    public List<String> validate(String value) {
         List<String> result = new ArrayList<>();
         for (StringRule rule : rules) {
             String validated = rule.validate(value);
-            if (!validated.equals("null"))
+            if (validated != null)
                 result.add(validated);
         }
         return result;
