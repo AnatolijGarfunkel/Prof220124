8df322d34b81bef3fb8d21e6987a7f69724c1b95
tolya <tolya@gmx.de>
2025-12-01 21:00:39 +0100

    class StringValidatorPredicateBuilder

--- src/main/java/field_validator/string_validator_predicate/StringRulesPredicate.java
+++ src/main/java/field_validator/string_validator_predicate/StringRulesPredicate.java
@@ -4,29 +4,29 @@ import java.util.function.Predicate;
 
 public class StringRulesPredicate {
 
-    public static Predicate<String> minLength(int length) {
-        return value -> value.length() < length;
-    }
-
-    public static Predicate<String>  onlyDigits() {
-        return value -> !value.chars().allMatch(Character::isDigit);
+    public static Predicate<String> minLength(int min) {
+        return value -> value.length() >= min;
     }
 
     public static Predicate<String>  maxLength(int max) {
-        return value -> value.length() > max;
+        return value -> value.length() <= max;
+    }
+
+    public static Predicate<String>  onlyDigits() {
+        return value -> value.chars().allMatch(Character::isDigit);
     }
 
     public static Predicate<String>  maxAge(int age) {
         return value -> {
             if (onlyDigits().test(value)) {
-                return Integer.parseInt(value) < 0 || Integer.parseInt(value) > age;
+                return Integer.parseInt(value) > 0 && Integer.parseInt(value) < age;
             }
             return false;
         };
     }
 
     public static Predicate<String>  beginsWithUp() {
-        return value -> !Character.isUpperCase(value.charAt(0));
+        return value -> Character.isUpperCase(value.charAt(0));
     }
 
     public static Predicate<String>  endsWithLow() {
