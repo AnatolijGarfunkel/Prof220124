7f259bb6ad26f0d0f78a7112afefd489803b21c9
tolya <tolya@gmx.de>
2025-12-15 20:42:28 +0100

    class Emp

---
+++ src/main/java/stream_basics/emp/Emp.java
@@ -0,0 +1,19 @@
+package stream_basics.emp;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Emp {
+
+    private String name;
+
+    private int age;
+
+    private String position;
+}
