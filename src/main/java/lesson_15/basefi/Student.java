e60f1cf394ce195befe7e5e9e81e9164cfe71c18
tolya <tolya@gmx.de>
2025-12-17 12:09:14 +0100

    class Student

---
+++ src/main/java/lesson_15/basefi/Student.java
@@ -0,0 +1,15 @@
+package lesson_15.basefi;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@ToString
+public class Student {
+
+    private String name;
+
+    private int age;
+}
