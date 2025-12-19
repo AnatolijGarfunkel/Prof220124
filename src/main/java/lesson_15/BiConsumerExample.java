0ba5a27252f4a981cfa7292e0631bf82fe981bc0
tolya <tolya@gmx.de>
2025-12-19 19:04:13 +0100

    main BiConsumerExample

---
+++ src/main/java/lesson_15/BiConsumerExample.java
@@ -0,0 +1,16 @@
+package lesson_15;
+
+import java.util.function.BiConsumer;
+
+public class BiConsumerExample {
+
+    public static void main(String[] args) {
+
+        BiConsumer<Integer, Integer> sum = (a, b) -> System.out.println(a + b);
+        sum.accept(10, 5);
+
+        BiConsumer<Integer, Integer> mult = (a, b)  -> System.out.println(a * b);
+        mult.accept(10, 5);
+        
+    }
+}
